7760df3da36ce8394567fef90febeacd1ec09bea
Danim Kim <kdn725@gmail.com>
2021-12-25 18:53:44 +0900

    💄 remove  Container  temporarily

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -6,14 +6,7 @@ export const Layout: React.FC = ({ children }) => {
   return (
     <div>
       <Navigation />
-      <Container>
         { children }
-      </Container>
     </div>
   )
 }
-
-const Container = styled.main`
-  max-width: 1280px;
-  margin: auto;
-`
