c7790c050ee8328ee43608fd5e23b65f9ad25ef0
Danim Kim <kdn725@gmail.com>
2021-12-26 03:08:22 +0900

    💄 update About page styles

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,9 +1,11 @@
 import { NextPage } from "next";
 import styled from "@emotion/styled";
+import { css } from "@emotion/css"
 import { fetcher } from '../utils/fetcher';
 import useSWR from 'swr';
 import { Error, Loading } from '../components'
 import { InfoData } from '../types/info';
+import Link from "next/link";
 
 const InfoPage: NextPage = () => {
     const { data, error } = useSWR('https://api.sampleapis.com/futurama/info', fetcher);
@@ -13,16 +15,25 @@ const InfoPage: NextPage = () => {
 
     return (
         <div>
-            <Button href="./">Back</Button>
-            <Heading>This is Info Page.</Heading>
+            <Image src="https://resizing.flixster.com/DXuKUIDKGNjh8PmejB406KwtOcQ=/fit-in/1152x864/v2/https://flxt.tmsimg.com/assets/p9932851_b_h9_ab.jpg" alt="Futurama Cover Image" />
             {data.map((info: InfoData) => {
                 const {synopsis, yearsAired, creators, id } = info;
     
                 return (
                     <div key={`futurama-showinfo-${id}`}>
-                        <Synopsis>synopsis: {synopsis}</Synopsis>
-                        <YearsAired>yearsAired: {yearsAired}</YearsAired>
-                        <p>{creators[0].name}</p>
+                        <Heading>Years Aired</Heading>
+                        <Content>{yearsAired}</Content>
+                        <Heading>Creators</Heading>
+                        <Content>
+                            <Link href={creators[0].url}>
+                                <a>{creators[0].name}, </a>
+                            </Link> 
+                            <Link href={creators[1].url}>
+                                <a>{creators[1].name}</a>
+                            </Link>
+                        </Content>
+                        <Heading>Synopsis</Heading>
+                        <Content>{synopsis}</Content>
                     </div>
                 )
             })}
@@ -30,35 +41,25 @@ const InfoPage: NextPage = () => {
     )
 }
 
-
-
-const Heading = styled.h1`
-    margin: 10px;
-    font-size: 30px;
+const Heading = styled.p`
+    font-size: 1.5rem;
     font-weight: 600;
-`
-
-const Button = styled.a`
+    border-bottom: solid 3px #5FD1D1;
     display: inline-block;
-    margin: 20px;
-    padding: 10px;
-    border-radius: 5px;
-    border: solid 1px black;
-    text-decoration: none;
-    color: black;
-    font-size: 15px;
-`
-
-const Synopsis = styled.p`
-    font-size: 18px;
-    line-height: 150%;
+    padding-bottom: 7px;
     margin-bottom: 10px;
 `
+const Image = styled.img`
+    display: block;
+    width: 40vw;
+    margin: 20px auto 50px;
+    box-shadow: 10px 13px 18px #aaa;
+`
 
-const YearsAired = styled.p`
-    font-size: 18px;
-    line-height: 150%;
-    margin-bottom: 10px;
+const Content = styled.p`
+    font-size: 1rem;
+    line-height: 180%;
+    margin-bottom: 20px;
 `
 
 export default InfoPage;
