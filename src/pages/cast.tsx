0103dda0edce7ef474b180da38a2cdff432f52e3
Danim Kim <kdn725@gmail.com>
2021-12-28 01:01:46 +0900

    💄 add some styles on CastCard and added cast images link

--- src/pages/cast.tsx
+++ src/pages/cast.tsx
@@ -4,48 +4,98 @@ import { fetcher } from '../utils/fetcher';
 import useSWR from 'swr';
 import { Error, Loading } from '../components'
 import { CastData } from "../types/cast";
+import Link from "next/link";
+import { API_ENDPOINT, CASTIMAGE_ENDPOINT, CASTIMAGE_ROUTES } from "../constants";
 
+interface CASTIMAGE_ROUTE {
+    ID: number,
+    PATH: string,
+    LABEL: string,
+}
 
 const CastPage: NextPage = () => {
-    const { data, error } = useSWR('https://api.sampleapis.com/futurama/cast', fetcher);
+    const route = 'cast';
+    const { data, error } = useSWR(`${API_ENDPOINT}${route}`, fetcher);
 
     if(error) return <Error />
     if(!data) return <Loading />
 
 
+
     return (
         <div>
-            {data.map((cast: CastData) => {
-                const {name, born, bio, id } = cast;
-    
-                return (
-                    <div key={`futurama-cast-${id}`}>
-                        <p>Name: {name}</p>
-                        <p>Born: {born}</p>
-                        <p>IMDb: <a href={bio.url}>{bio.url}</a></p>
-                    </div>
-                )
-            })}
+            <Container>
+                {data.map((cast: CastData) => {
+                    const {name, born, bio, id } = cast;
+                    
+                    return (
+                        <Card key={`futurama-cast-${id}`}>
+                            <Link href={bio.url}>
+                                <a   target="_blank" rel="noopener noreferrer">
+                                    <Image src={`${CASTIMAGE_ENDPOINT}${CASTIMAGE_ROUTES[id-1].PATH}`} alt="" />
+                                    <div>
+                                        <Name>{name}</Name><Icon>IMDb</Icon>
+                                    </div>
+                                    <p><Subitem>Born</Subitem> {born}</p>
+                                </a>
+                                </Link>
+                        </Card>
+                    )
+                })}
+            </Container>
         </div>
     )
 }
 
 
-const Heading = styled.h1`
-    margin: 10px;
-    font-size: 30px;
+const Name = styled.h1`
+    display: inline-block;
+    margin: 10px 5px;
+    font-size: 1.2rem;
     font-weight: 600;
 `
-
-const Button = styled.a`
-    display: inline-block;
-    margin: 20px;
-    padding: 10px;
-    border-radius: 5px;
-    border: solid 1px black;
-    text-decoration: none;
-    color: black;
-    font-size: 15px;
+const Icon = styled.span`
+    border: solid 1px #EDBF17;
+    background-color: #EDBF17;
+    font-weight: 500;
+    padding: 3px;
+    border-radius: 3px;
+    font-size: 0.7rem;
+    margin-bottom: 10px;
 `
+
+const Subitem = styled.span`
+    font-weight: 500;
+`
+
+const Container = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+`
+const Card = styled.div`
+    border: solid 2px #f2f2f2;
+    border-radius: 5px;
+    min-width: min-content;
+    width: 20vw;
+    text-align: center;
+    padding: 10px;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    margin: 0 auto;
+    &:hover{
+        box-shadow: 0px 0px 5px #aaa;
+        transition: .2s;
+    }
+`
+
+const Image = styled.img`
+    width: 214px;
+`
+
+
+
 
 export default CastPage;
