e8a09ead941f4d8c311a7a58c5aecfb8454ae1b8
Danim Kim <kdn725@gmail.com>
2021-12-28 01:58:56 +0900

    💄 added image and button for Quiz Intro page

--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -20,11 +20,13 @@ const QuestionsPage: NextPage = () => {
         <div>
             {!visible && 
             <div>
-                <Title>Pop Quiz!</Title>
-                <img src="" alt="Quiz Intro Image" />
-                <button onClick={() => {
+                <ImageContainer>
+                    <Image src="https://drewscave.files.wordpress.com/2014/12/futurama-logo-with-characters.png" alt="Quiz Intro Image" />
+                    <TextImage>Quiz!</TextImage>
+                </ImageContainer>
+                <Button onClick={() => {
                     setVisible(!visible)
-                }}>Start</button>
+                }}>Start</Button>
             </div>
             }
             {visible && <Quiz />}
@@ -32,8 +34,48 @@ const QuestionsPage: NextPage = () => {
     )
 }
 
-export default QuestionsPage;
 
-const Title = styled.h1`
-    font-size: 1.5rem;
+
+
+const Image = styled.img`
+    display: block;
+    margin: 0 auto;
+    height: 60vh;
+    margin: 10vh auto 0;
 `
+
+const TextImage = styled.p`
+    position: absolute;
+    font-size: 8em;
+    right: 300px;
+    bottom: 0px;
+    transform: rotate(-20deg) skew(20deg);
+    padding: 5px;
+    color: #F1FF8E;
+    text-shadow:  -5px -5px 0 #DA171B,
+    1px -1px 0 #BE1419,
+    -5px 5px 0 #000,
+    1px 1px 0 #000;
+`
+
+const ImageContainer = styled.div`
+    position: relative;
+    min-width: 1200px;
+`
+const Button = styled.button`
+    display: block;
+    margin: 0 auto;
+    box-sizing: border-box;
+    font-size: 2rem;
+    padding: 10px 15px;
+    border-radius: 5px;
+    background: radial-gradient(#DD171B,#8D1015);
+    color: #FDFE95;
+    border: solid 1px #8D1015;
+    &:hover{
+        cursor: pointer;
+        background: radial-gradient(#8D1015,#DD171B);
+        border: solid 1px #DD171B;
+    }
+`
+export default QuestionsPage;
