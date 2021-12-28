64762b8cd59edb4cb712f43c8b996855f3f2cc4a
Danim Kim <kdn725@gmail.com>
2021-12-28 13:01:37 +0900

    💄 added styles

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -4,33 +4,47 @@ import { API_ENDPOINT } from '../constants';
 import styled from "@emotion/styled";
 import { fetcher } from '../utils/fetcher';
 import { QuizData } from '../types/questions';
+import { useState } from 'react';
 
 export const Quiz = () => {
     const route = 'questions';
     const { data, error } = useSWR(`${API_ENDPOINT}${route}`, fetcher);
-    
+    const [visible, setVisible] = useState(false);
+
     if(error) return <Error />
     if(!data) return <Loading />
 
+    
+    const generateRandomNum = () => {
+        let randomNum = Math.floor(Math.random() * data.length +1);
+        console.log(randomNum);
+    }
     return (
         <div>
             <Container>
                 {data.map((quiz: QuizData) => {
                 const { id, question, possibleAnswers, correctAnswer } = quiz;
 
+
                     return (
-                        <div key={`futurama-quiz-${id}`}>
-                            <p key={`quiz-question-${id}`}>{question}</p>
-                            <div key={`quiz-possibleAnswer-${id}`}>
-                                {possibleAnswers.map((posAnswer: string) => {
-                                    return (
-                                        <div key={`possibleAnswer-option-${possibleAnswers.indexOf(posAnswer)}`}>
-                                            <input type="radio" id={posAnswer} name={`${id}`} value={posAnswer} />
-                                            <label htmlFor={posAnswer}>{posAnswer}</label>
-                                        </div>
-                                    )
-                                })}
-                            </div>
+                        <div>
+                            <QuizCard key={`futurama-quiz-${id}`}>
+                                <Question key={`quiz-question-${id}`}>{question}</Question>
+                                <div key={`quiz-possibleAnswer-${id}`}>
+                                    {possibleAnswers.map((posAnswer: string) => {
+                                        return (
+                                            <div key={`possibleAnswer-option-${possibleAnswers.indexOf(posAnswer)}`}>
+                                                <input type="radio" id={posAnswer} name={`${id}`} value={posAnswer} />
+                                                <label htmlFor={posAnswer}>{posAnswer}</label>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            </QuizCard>
+                            {/* <button onClick={() => {
+                                setVisible(!visible)
+                                generateRandomNum()
+                            }}>Next</button> */}
                         </div>
                     )
                 })}
@@ -40,6 +54,20 @@ export const Quiz = () => {
 }
 
 const Container = styled.div`
-    border: solid 1px black;
+`
+const Question = styled.h1`
+    font-weight: 500;
+    font-size: 1rem;
+    margin-bottom: 10px;
+`
+
+
+const QuizCard = styled.div`
+    border: solid 1px rgb(103, 217, 219, 20%);
+    background-color: rgb(103, 217, 219, 20%);
+    margin: 10px;
+    box-sizing: border-box;
+    padding: 20px;
+    border-radius: 10px;
 `
 
